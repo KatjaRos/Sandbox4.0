e121e28c706095dcaa40c943cac837860e5724d7
KatjaRos <129310809+KatjaRos@users.noreply.github.com>
2023-06-19 10:12:49 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+
+const btnSubmit = document.getElementById("submit-data");
+btnSubmit.addEventListener("click", submitData);
+async function submitData() {
+  const body = document.getElementById("b");
+  const search = document.getElementById("input-show").value;
+  let url1 = "https://api.tvmaze.com/search/shows?q=";
+  const url = url1 + search;
+  console.log(url);
+  const data = await fetch(url);
+  const tv_data = await data.json();
+  const values = tv_data;
+  console.log(values);
+  for (var i = 0; i < values.length; i++) {
+    let div1 = document.createElement("div");
+    div1.setAttribute("class", "show-data");
+    let img = document.createElement("img");
+    img.setAttribute("src", "values[i].image.medium");
+    let div2 = document.createElement("div");
+    div2.setAttribute("class", "show-info");
+    let h1 = document.createElement("h1");
+    h1.setAttribute("id", "show-title");
+    h1.innerHTML = values[i].show.name;
+    console.log(values[i].show.name);
+    div2.appendChild(h1);
+    let p = document.createElement("div");
+    p.innerHTML = values[i].show.summary;
+    div2.appendChild(p);
+    div1.appendChild(img);
+    div1.appendChild(div2);
+    console.log(img.src);
+    body.appendChild(div1);
+  }
+}
